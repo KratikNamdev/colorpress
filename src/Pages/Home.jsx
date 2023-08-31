633a994f28702badef33b410fb716eeb2c31607d
Kratik <kratiknamdev47@gmail.com>
2023-08-31 19:04:05 +0530

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,44 +1,32 @@
 import React from 'react'
-import Navbar from '../Components/Navbar';
-import NavCategory from '../Components/NavCategory';
-import Banner from '../Components/Banner';
-import HomeCards from '../Components/HomeCards';
-import TodayDeal from '../Components/TodayDeal';
-import Grid from '../Components/Grid';
-import Footer from '../Components/Footer';
-import Explore from '../Components/Explore';
+import Navbar from '../Components/Navbar'
+import Banner from '../Components/Banner'
+import NavCategory from '../Components/NavCategory'
+import HomeCards from '../Components/HomeCards'
+import TodayDeal from '../Components/TodayDeal'
+import Grid from '../Components/Grid'
+import Explore from '../Components/Explore'
+import Footer from '../Components/Footer'
+
 
 function Home() {
   return (
-   <>
-       <Navbar/>
-      <NavCategory/>
-      <br/>
-      <Banner/>
-      <HomeCards/>
-      {/* <br></br> */}
-      <HomeCards/>
-      <br></br>
-      <HomeCards/>
-      <br></br>
-      <br></br>
+<>
+    <Navbar/>
+    <NavCategory/>
+    <br/>
+    <Banner/>
+    <HomeCards/>
+    <TodayDeal/>
+    <br/>
 
-      <TodayDeal/>
-      <br/>
-      <Grid/>
-      <Explore/>
-      <br/>
-      <br/>
-      <br/>
-
-      <h1 className='browse' >Your browsing history</h1>
-      <HomeCards/>
-      <br></br>
-      <br></br>
-      <br></br>
-
-      <Footer/>
-   </>
+    <Grid/>
+    <Explore/>
+    <br/>
+    <h1 className='browse'> Your Browsing History</h1>
+    <HomeCards/>
+    <Footer/>
+</>
   )
 }
 
